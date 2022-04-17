001b522c21048b34bb9e4e178707531df86ced19
Nicolas Vauche <hello@nicolasvauche.net>
2022-04-17 16:48:56 +0000

    Random suggestions

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\AlbumRepository;
 use App\Repository\ArtistRepository;
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,17 +16,26 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(CategoryRepository $categoryRepository, ArtistRepository $artistRepository, AlbumRepository $albumRepository): Response
     {
-        $albums = $albumRepository->findAll();
-        shuffle($albums);
-        $suggestedAlbum = array_slice($albums, 0, 4);
-        $suggestedAlbum = $suggestedAlbum;
+        $categories = $categoryRepository->findAll();
+        $suggestedCategories = new ArrayCollection();
+        foreach ($categories as $category) {
+            $suggestedCategory = new Category();
+            $albums = $category->getAlbums()->toArray();
+            shuffle($albums);
+            $randomAlbum = array_slice($albums, 0, 1);
+            $suggestedCategory
+                ->setName($category->getName())
+                ->setSlug($category->getSlug())
+                ->addAlbum($randomAlbum[0]);
+            $suggestedCategories->add($suggestedCategory);
+        }
 
         $albumsNeverListened = $albumRepository->findByNeverListened();
         shuffle($albumsNeverListened);
         $albumsNeverListened = array_slice($albumsNeverListened, 0, 8);
 
         return $this->render('default/index.html.twig', [
-            'suggestedAlbum' => $suggestedAlbum,
+            'suggestedCategories' => $suggestedCategories,
             'categories' => $categoryRepository->findBy([], ['name' => 'ASC']),
             'artists' => $artistRepository->findBy([], ['name' => 'ASC']),
             'albumsPlayed' => $albumRepository->findByMostPlayed(),
